8759616430ed579b9bc1d3653a84f0ad108bc035
Victor Butler <victorbutler@gmail.com>
2012-06-24 18:22:04 -0400

    oops. new form

--- views/projects/new.php
+++ views/projects/new.php
@@ -1,3 +1,11 @@
-<form>
-	<label>Project Name<input type="text" name="project_name" /></label>
+<h1>Add a New Project</h1>
+<form action="<?=url_for('projects/new')?>" method="post">
+	<div class="well">
+		<h2>Repository</h2>
+		<label><span class="span2">Name</span> <input type="text" name="repositroy_name" /></label>
+		<label><span class="span2">Remote Origin</span> <input type="text" name="repository_remote" /></label>
+	</div>
+	<label><span class="span2">Project Name</span> <input type="text" name="project_name" /></label>
+	<label><span class="span2">Branch</span> <input type="text" name="project_branch" /></label>
+	<input type="submit" class="btn btn-primary" value="Create"/>
 </form>
