466546d09eea5e4e479beb1640843700a5ef37a8
Angelo <manciniangelo@gmail.com.com>
2012-06-24 19:16:05 -0400

    Mixing a select box with an input field
    Mixing a select box with an input field
    
    Signed-off-by: Angelo <manciniangelo@gmail.com.com>

--- views/projects/new.php
+++ views/projects/new.php
@@ -1,11 +1,34 @@
 <h1>Add a New Project</h1>
 <form action="<?=url_for('projects/new')?>" method="post">
-	<div class="well">
-		<h2>Repository</h2>
-		<label><span class="span2">Name</span> <input type="text" name="repositroy_name" /></label>
-		<label><span class="span2">Remote Origin</span> <input type="text" name="repository_remote" /></label>
+	<h2>Repository</h2>
+    
+    <div class="well">
+    <div class="control-group">
+      <label class="control-label" for="appendedInputButton">Append with button</label>
+      <div class="controls">
+        <div class="input-append btn-group">
+          <input class="span5" id="appendedInputButton" size="16" type="text" style="float:left;">
+          <a class="btn dropdown-toggle span3" data-toggle="dropdown" href="#">Select an existing repository
+    		<span class="caret"></span>
+  		  </a>
+          <ul class="dropdown-menu span8">
+            <li><a href="#">Switch Rabbit</a></li>
+            <li><a href="#">Wonder Lime</a></li>
+            <li><a href="#">TIAA-CREF</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+    
+    <div class="well">
+		<label><span class="span2">Name:</span> <input type="text" name="repositroy_name" /></label>
+		<label><span class="span2">Remote Origin:</span> <input type="text" name="repository_remote" /></label>
 	</div>
-	<label><span class="span2">Project Name</span> <input type="text" name="project_name" /></label>
-	<label><span class="span2">Branch</span> <input type="text" name="project_branch" /></label>
+    <h2>Project</h2>
+    <div class="well">
+	<label><span class="span2">Project Name:</span> <input type="text" name="project_name" /></label>
+	<label><span class="span2">Branch:</span> <input type="text" name="project_branch" /></label>
+    </div>
 	<input type="submit" class="btn btn-primary" value="Create"/>
 </form>
