9a8212d9a293c6b0cc5efc7a1e66fe9158bf21a7
Victor Butler <victorbutler@gmail.com>
2012-06-25 05:16:20 -0400

    activated nav link

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,7 +3,15 @@
 		<div class="container">
 			<a class="brand" href="<?=url_for('/')?>">Git Deploy</a>
 			<ul class="nav">
-				<li><a href="<?=url_for('/projects')?>"><span class="badge"><?=$num_of_repositories?></span> Projects</a></li>
+				<li<?=(request_uri() == '/projects' ? ' class="active"' : '')?>>
+					<a href="<?=url_for('/projects')?>">
+<?php
+if ($num_of_repositories):
+?>
+					<span class="badge"><?=$num_of_repositories?></span>
+<?php
+endif;
+?>					 Projects</a></li>
 				<!-- <li><a href="<?=url_for('/activity')?>"><span class="badge badge-success">2</span> Activity</a></li> -->
 			</ul>
 		</div>
