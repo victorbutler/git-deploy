ba04049b7a35ff9515009188de3c0c8eac55b0c6
Victor Butler <victorbutler@gmail.com>
2012-07-02 15:22:10 -0400

    put my twitter up instead of my tiaa email

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,5 +1,5 @@
 		<footer class="footer">
 			<p class="pull-right"><a href="https://github.com/victorbutler/git-deploy">Github</a></p>
-			<p>Git Deploy for Prototyping &mdash; Developed by <a href="mailto:vbutler@tiaa-cref.org">Victor Butler</a> &copy; 2012</p>
+			<p>Git Deploy for Prototyping &mdash; Developed by <a href="http://twitter.com/victor_butler">Victor Butler</a> &copy; 2012</p>
 			<p>Built using <a href="http://limonade-php.github.com/">Limonade</a> and Twitter's <a href="http://twitter.github.com/bootstrap/">Bootstrap</a>.</p>
 		</footer>
