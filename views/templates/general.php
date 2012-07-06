715aa57f6e80e93f25a3dce5c8e18b1eb9f2ca99
Victor Butler <victorbutler@gmail.com>
2012-07-06 10:34:08 -0400

    device responsiveness

--- views/templates/general.php
+++ views/templates/general.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta http-equiv="imagetoolbar" content="false">
 	<title><?php if (isset($title)) echo $title; else echo 'Deploy System'; ?></title>
